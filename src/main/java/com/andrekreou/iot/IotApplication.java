19e7d8252fa5461f3a513b91c57ad4972870b5c9
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2022-10-05 14:06:15 +0300

    Added custom timer dependency and created TimedAspect bean on main

--- src/main/java/com/andrekreou/iot/IotApplication.java
+++ src/main/java/com/andrekreou/iot/IotApplication.java
@@ -1,7 +1,10 @@
 package com.andrekreou.iot;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -10,5 +13,14 @@ public class IotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IotApplication.class, args);
+	}
+
+	/**
+	 * This is required so that we can use the @Timed annotation
+	 * on methods that we want to time.
+	 */
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
 	}
 }
