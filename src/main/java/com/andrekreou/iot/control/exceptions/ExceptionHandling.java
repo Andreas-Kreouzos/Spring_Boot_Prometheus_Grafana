737a5ebd7f9358d44c54b441daecdaa9753c3f3b
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2022-10-09 19:39:01 +0300

    Added Exception Handling Methods with Response Entity

---
+++ src/main/java/com/andrekreou/iot/control/exceptions/ExceptionHandling.java
@@ -0,0 +1,21 @@
+package com.andrekreou.iot.control.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandling extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<Object> handleConflict(IllegalStateException exception) {
+        String error = "Error: " + exception.getMessage();
+        return buildResponseEntity(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, error));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ErrorResponse errorResponse){
+        return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
+    }
+}
