f80ad50d86fb2c58b1e18aa2433eb72effee9445
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2022-11-08 19:49:16 +0200

    Changed the Application User Roles

--- src/main/java/com/andrekreou/iot/authentication/security/ApplicationUserRole.java
+++ src/main/java/com/andrekreou/iot/authentication/security/ApplicationUserRole.java
@@ -1,34 +1,6 @@
 package com.andrekreou.iot.authentication.security;
 
-import com.google.common.collect.Sets;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static com.andrekreou.iot.authentication.security.ApplicationUserPermission.MOVIES_VIEW;
-import static com.andrekreou.iot.authentication.security.ApplicationUserPermission.NEWS_VIEW;
-
 public enum ApplicationUserRole {
-    ADMIN(Sets.newHashSet(NEWS_VIEW,MOVIES_VIEW)),
-    USER(Sets.newHashSet());
-
-    private final Set<ApplicationUserPermission> permissions;
-
-    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
-        this.permissions = permissions;
-    }
-
-    public Set<ApplicationUserPermission> getPermissions() {
-        return permissions;
-    }
-
-    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
-        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-                .collect(Collectors.toSet());
-        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
-
-        return permissions;
-    }
+    ADMIN,
+    USER
 }
