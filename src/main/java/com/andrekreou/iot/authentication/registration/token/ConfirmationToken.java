55428ebea689fcc921cf7ea2e939613561cee90b
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2023-04-28 12:58:30 +0300

    Refactored ConfirmationToken class by removing unnecessary annotations

--- src/main/java/com/andrekreou/iot/authentication/registration/token/ConfirmationToken.java
+++ src/main/java/com/andrekreou/iot/authentication/registration/token/ConfirmationToken.java
@@ -1,18 +1,21 @@
 package com.andrekreou.iot.authentication.registration.token;
 
 import com.andrekreou.iot.authentication.user.ApplicationUser;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
-@EqualsAndHashCode
 @Entity
 public class ConfirmationToken {
 
