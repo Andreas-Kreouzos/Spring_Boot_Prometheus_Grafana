62f4ce2fb675cad4171a7ae8c84d7b67f258ed24
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2024-11-03 14:00:35 +0200

    Minor fixes

--- src/main/java/com/andrekreou/iot/repository/CryptoNewsRepo.java
+++ src/main/java/com/andrekreou/iot/repository/CryptoNewsRepo.java
@@ -4,11 +4,7 @@ import com.andrekreou.iot.entity.CryptoNews;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//Interface that multiple classes can use and connects with data JPA
-//The JPA is a map that takes the variables mapped in Movies class
-//as first parameter and as second, returns the data type of the Id.
 @Repository
-public interface CryptoNewsRepo
-        extends JpaRepository<CryptoNews,Integer> {
+public interface CryptoNewsRepo extends JpaRepository<CryptoNews, Integer> {
 
 }
